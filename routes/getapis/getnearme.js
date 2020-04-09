a1f999be2e4e5a9bf6d67528ccf1130b56e50aeb
dineshchoudhary1998 <50098434+dineshchoudhary1998@users.noreply.github.com>
2020-04-09 10:44:26 +0530

    Nearme updated

--- routes/getapis/getnearme.js
+++ routes/getapis/getnearme.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
    
         vendor_object= await Vendordetails.find({})
         
-      //Likes
+   /*   //Likes
         function Likes(video)
         {
           var Likes=[]
@@ -28,15 +28,7 @@ router.get('/', async (req, res) => {
           return (Url)
 
         }
-        //Comments
-        function No_of_Comments(comment)
-        {    var count=0
-            comment.map(mapvar=>{
-                    count++
-            })
-            return count
-        }
-
+     
         function Comments(video)
         { var count_array=[]
           video.map(mapvar=>{
@@ -44,14 +36,7 @@ router.get('/', async (req, res) => {
           })
           return count_array
         }
-        //Reviews
-        function No_of_Reviews(review)
-        {    var count=0
-            review.map(mapvar=>{
-                    count++
-            })
-            return count
-        }
+    
 
         function Reviews(video)
         { var count_array=[]
@@ -60,20 +45,63 @@ router.get('/', async (req, res) => {
           })
           return count_array
         }
-    
+       
+        function Getid(video)
+        {
+          var id=[]
+          console.log()
+          video.map(mapvar=>{
+                id.push(mapvar._id)
+          })
+          return (id)
+
+        }
+        */
+
+        //comments
+       function No_of_Comments(comment)
+       {    var count=0
+           comment.map(mapvar=>{
+                   count++
+           })
+           return count
+       }
+
+        //Reviews
+           function No_of_Reviews(review)
+           {    var count=0
+               review.map(mapvar=>{
+                       count++
+               })
+               return count
+           }
+
+
+        function Videostats(video)
+        {
+          var stats=[]
+          video.map(mapvar=>{
+           stats.push( { 
+              "video_id": mapvar._id,
+              "Likes":mapvar.Likes,
+              "No_of_comments":No_of_Comments(mapvar.Comments),
+              "No_of_reviews":No_of_Reviews(mapvar.Reviews),
+              "Video_url":mapvar.Url
+        })
+          })
+          return stats
+        }
        //Data which is returned as json in nearby api
        
         var required__object=[]
         vendor_object.map(vendor=>{
                 required__object.push({
+                    "Vendor_id":vendor._id,
                     "Vendor_name":vendor.Vendor_name,
                     "Followers":vendor.Followers,
-                    "videostats":{ 
-                    "Likes":Likes(vendor.Videos),
-                    "No_of_comments":Comments(vendor.Videos),
-                    "No_of_reviews":Reviews(vendor.Videos),
-                    "Video_url":Url(vendor.Videos)
-                  }
+                    "Videostats":Videostats(vendor.Videos),
+                    "Location":vendor.Location
+                 
                })
           
         })
