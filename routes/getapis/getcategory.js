6c1bec14438bb32d0c634415de7c83624cfa5c65
dineshchoudhary1998 <50098434+dineshchoudhary1998@users.noreply.github.com>
2020-04-12 14:41:29 +0530

    id

--- routes/getapis/getcategory.js
+++ routes/getapis/getcategory.js
@@ -10,17 +10,25 @@ router.get('/', getcategory , async(req,result)=>{
 async function getcategory(req,res,next){
   
       try {
-       
         vendor_object= await Vendordetails.find({})
         const category=[]
         vendor_object.map(vendor=>{
           vendor.Videos.map(video=>{
-           category.push(video.Category)
+           category.push(
+             //"id":Math.random().toString(36).slice(2),
+             video.Category)
           })    
         })
-        var  unique= Array.from(new Set(category))  
+        var  unique= Array.from(new Set(category)) 
+        var unique_array=[] 
+        for (const prop in unique){
+            unique_array.push({
+              "id":Math.random().toString(36).slice(2),
+              "Category":unique[prop]
+            })
+        }
         console.log("--------------------------------------------------------------------------------------------------")
-        console.log(unique)
+        console.log(unique_array)
         if (category == null) {
           return res.status(404).json({ message: 'Cant find vendor'})
         }
@@ -28,7 +36,7 @@ async function getcategory(req,res,next){
       } catch(err){
         return res.status(500).json({ message: err.message })
       }
-      res.category=unique
+      res.category=unique_array
       next()
 }
 
